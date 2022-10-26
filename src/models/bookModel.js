cae63c8570c432ae0ec13449bd2d6d2eca25160e
AbhijitRadke <radkeabhi27@gmail.com>
2022-10-26 18:18:19 +0530

    Book problem 1

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
+const bookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    authorName: String,
     tags: [String],
-    
+    year: { type: Number, default: 2021 },
+    totalPages: Number,
+    stockAvailable: Boolean,
     isPublished: Boolean,
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    sales: { type: Number, default: 10 }
 }, { timestamps: true });
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const BookController = require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+router.post("/createBook", BookController.createBook)
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/bookList", BookController.getBooksData)
+
+router.post("/getBooksInYear", BookController.getWithYear)
+
+router.post("/getParticularBooks", BookController.getWithany)
+
+router.get("/getXINRBooks", BookController.getWithPrise)
+
+router.get("/getRandomBooks", BookController.getWithPages)
 
 module.exports = router;
