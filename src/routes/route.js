4a273f992f95ece81352f818d6d42a2565ad3da8
AbhijitRadke <radkeabhi27@gmail.com>
2022-11-06 09:45:19 +0530

    Authorization with jwt

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const userController= require("../controllers/userController")
+const userController = require("../controllers/userController")
+const VarifyMid = require("../medelware/authMid")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/users", userController.createUser)
+
+router.post("/signIn", userController.createUser)
 
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.get("/users/:userId", VarifyMid.authetication, VarifyMid.Authorization, userController.getUserData)
+
+router.put("/users/:userId", VarifyMid.authetication, VarifyMid.Authorization, userController.updateUser)
+
+router.delete("/users/:userId", VarifyMid.authetication, VarifyMid.Authorization, userController.deleteUser)
+
 
 module.exports = router;
